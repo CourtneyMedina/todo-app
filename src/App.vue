677c95e268be606445c4f3041c7dd0cde228f3de
CourtneyMedina <courtney.medina.222@gmail.com>
2020-10-03 22:12:35 -0500

    third commit update

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,70 @@
 <template>
+  <h2>Vue 3 Todo App</h2>
+    <form @submit.prevent="addNewTodo">
+      <label>New Todo List</label>
+      <input v-model="newTodo" name="newTodo">
+      <button>Add New Task</button>
+    </form>
+    <button @click="strikeAll">Strike All</button>
+    <button @click="removeAll">Remove All</button>
+    <div>   
+      <ul>
+        <li v-for="(todo, index ) in todos" v-bind:key="todo.id" class="todo"> <!-- iterates over unique elements in the array 'todos' -->
+          <h4 @click="toggleDone(todo)" :class="{ done: todo.done }">{{todo.content}}</h4>    <!-- "{ done: todo.done }" Shorthand for "{ done: todo.done == true }", and applies the .done css -->
+          <button @click="removeTodo(index)">Delete</button>
+        </li>
+      </ul>
+    </div>
 
 </template>
 
 <script>
-
+import { ref } from 'vue';              // Imports 'ref' library
 
 export default {
-  name: 'App',
-  components: {
-    todo
+  setup(){
+    const newTodo = ref('');            // Creates a v-model with empty reactive element
+    const todos = ref([]);
+
+    function addNewTodo(){
+      console.log(newTodo.value);
+      todos.value.push({
+        id: Date.now(),                 // Needed for unique keys in iteration
+        done: false,
+        content: newTodo.value,
+      });
+      newTodo.value = '';                // Changes to the v-model also changes the field, & vis a vis (similar to "2-way binding")
+    }
+    
+    function toggleDone(todo){
+      todo.done = !todo.done;
+    }
+
+    function removeTodo(index){
+      todos.value.splice(index, 1)
+    }
+
+    function strikeAll(){
+      todos.value.forEach((todo) => todo.done = true);
+    }
+
+    function removeAll(){
+      todos.value = [];
+    }
+
+
+    return {
+      addNewTodo,                     // Exposes these variables to the v-model
+      newTodo,
+      todos,
+      toggleDone,
+      removeTodo,
+      strikeAll,
+      removeAll,
+    }
+
+    
+
   }
 }
 </script>
@@ -35,4 +91,5 @@ input, textarea, button, p, div, section, article, select {
 .done {
   text-decoration: line-through;
 }
+
 </style>
