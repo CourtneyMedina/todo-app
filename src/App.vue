957c939e66e5ba5317dfae80c39e9f9b4fce83e7
CourtneyMedina <courtney.medina.222@gmail.com>
2020-10-05 23:12:01 -0500

    added icon to delete button; improved css

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,17 @@
     <form @submit.prevent="addNewTodo">
       <label>New Todo List</label>
       <input v-model="newTodo" name="newTodo">
-      <button>Add New Task</button>
+      <button class="btn btn-outline-primary">Add New Task</button>
     </form>
-    <button @click="strikeAll">Strike All</button>
-    <button @click="removeAll">Remove All</button>
+    <button class="btn btn-outline-success" @click="strikeAll">Strike All</button>
+    <button class="btn btn-outline-danger" @click="removeAll">Remove All</button>
     <div>   
       <ul>
         <li v-for="(todo, index ) in todos" v-bind:key="todo.id" class="todo"> <!-- iterates over unique elements in the array 'todos' -->
           <h4 @click="toggleDone(todo)" :class="{ done: todo.done }">{{todo.content}}</h4>    <!-- "{ done: todo.done }" Shorthand for "{ done: todo.done == true }", and applies the .done css -->
-          <button @click="removeTodo(index)">Delete</button>
+          <button class="btn btn-outline-danger btn-sm" @click="removeTodo(index)"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+          <path fill-rule="evenodd" d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5a.5.5 0 0 0-1 0v7a.5.5 0 0 0 1 0v-7z"/>
+          </svg></button>
         </li>
       </ul>
     </div>
@@ -92,4 +94,7 @@ input, textarea, button, p, div, section, article, select {
   text-decoration: line-through;
 }
 
+.btn-sm {
+  width: 2.5rem;
+}
 </style>
